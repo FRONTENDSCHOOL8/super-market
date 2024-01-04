50a57b6ce31daeb20cb771dcc160788ead8953a6
BELLORI <bellori729@gmail.com>
2024-01-04 17:31:15 +0900

    chore(feat/kim/#2): vite 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        
+        products: resolve(__dirname, 'src/pages/products/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        register: resolve(__dirname, 'src/pages/register/index.html'),
+        detail: resolve(__dirname, 'src/pages/detail/index.html'),
+        cart: resolve(__dirname, 'src/pages/cart/index.html'),
       },
     },
-    
   },
-})
+});
