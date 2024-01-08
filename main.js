d29e7b068c21767261a4f14c76b354b017249340
ryukyunghye <ryukyung123@naver.com>
2024-01-08 16:46:48 +0900

    feat(feat/ryu/#18): 메인 페이지 배너 스와이퍼 완성 - html: 스와이퍼에 필요한 link, script 추가 및 클래스명 수정 - scss: 필요없는 속성 제거 및 클래스 명 수정 - javascript: 메인 페이지 배너 스와이퍼 완성

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+import './src/styles/style.scss';
 
-
-import './src/styles/style.scss'
+const bannerSwiper = new Swiper('.banner.swiper-container', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 500,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
