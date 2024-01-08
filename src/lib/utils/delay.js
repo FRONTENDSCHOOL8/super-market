1b6c323a85b0ddd32f21cbab4b021240510056b7
SWLee2973 <kek2973@naver.com>
2024-01-08 17:32:06 +0900

    bugfix(feat/sang/#22): 불필요한 코드 및 주석 제거

--- src/lib/utils/delay.js
+++ src/lib/utils/delay.js
@@ -14,27 +14,6 @@ function delay(callback,timeout = 1000){
 }
 
 
-
-const first = getNode('.first')
-const second = getNode('.second')
-
-
-
-// delay(()=>{
-//   first.style.top = '-100px';
-//   delay(()=>{
-//     first.style.transform = 'rotate(360deg)'
-//     delay(()=>{
-//       first.style.top = '0px';
-//       second.style.top = '0px';
-//     })
-//     second.style.transform = 'rotate(-360deg)'
-//   })
-//   second.style.top = '100px';
-// })
-
-
-
 // Promise API
 
 const defaultOptions = {
@@ -71,40 +50,6 @@ export function delayP(options){
 }
 
 
-delayP(1000) // number | object
-
-
-// .then((res)=>{
-//   console.log( res );
-// })
-
-
-// .then((res)=>{
-//     console.log(res);
-//     return delayP()
-// })
-// .then((res)=>{
-//     console.log(res);
-//     return delayP()
-// })
-// .then((res)=>{
-//     console.log(res);
-//     return delayP()
-// })
-// .then((res)=>{
-//     console.log(res);
-//     return delayP()
-// })
-// .then((res)=>{
-//     console.log(res);
-// })
-
-
-
-
-
-
-// console.log( delayP(false) );
 
 
 
@@ -121,96 +66,6 @@ delayP(1000) // number | object
 async function delayA(data){
   return data
 }
-
-
-const value = await delayA('이슬기나');
-
-
-// value.then((res)=>{
-//   console.log( res );
-// })
-
-// console.log( value ); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function 라면끓이기(){
-
-  const 물 = await delayP({data:'물'});
-  console.log(물);
-
-  const 스프 = await delayP({data:'스프'});
-  console.log(스프);
-
-  const 면 = await delayP({data:'면'});
-  console.log(면);
-
-  const 그릇 = await delayP({data:'그릇'});
-  console.log(그릇);
-
-
-  // delayP({data:'물'})
-  // .then((res)=>{
-  //   console.log( res );
-
-  //   return delayP({data:'스프'})
-  // })
-  // .then((res)=>{
-  //   console.log( res );
-  //   return delayP({data:'면넣기'})
-  // })
-  // .then((res)=>{
-  //   console.log( res );
-  //   return delayP({data:'그릇에담기'})
-  // })
-  // .then((res)=>{
-  //   console.log( res );
-  // })
-
-
-
-
-  // console.log('물넣기');
-  // console.log('스프넣기');
-  // console.log('면넣기');
-  // console.log('그릇에담기');
-}
-
-
-
-// 라면끓이기()
-
-
-
-
-async function getData(){
-  
-  const data = await xhrPromise.get('https://pokeapi.co/api/v2/pokemon/15');
-
-  console.log( data );
-
-  
-  insertLast(document.body,`<img src="${data.sprites['front_default']}" alt="독침붕" />`)
-
-  console.log( data.sprites['front_default'] );
-
-}
-
-
-
-// getData()
 
 
 
