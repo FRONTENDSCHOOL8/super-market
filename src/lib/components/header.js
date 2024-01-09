249f7db4f87944599396e7acb610e4c512985fe2
SWLee2973 <kek2973@naver.com>
2024-01-09 15:39:47 +0900

    feat(feat/sang/#37): 배송지 입력 박스 기능 토글 구현

--- src/lib/components/header.js
+++ src/lib/components/header.js
@@ -1,7 +1,13 @@
+import { insertLast, getNode } from '/src/lib';
+
 const header = document.querySelector('.header');
 const headerMenubar = document.querySelector('.menu');
 
-const handleHeaderScroll = e => {
+const menuLink = document.querySelector('.menu_link')
+const addressButton = document.querySelector('.menu_link__address');
+let isShowAddressBox = false;
+
+const handleScrollHeader = e => {
 
   const currentPosition = window.scrollY;
   const headerPosition = header.getBoundingClientRect().height
@@ -15,5 +21,37 @@ const handleHeaderScroll = e => {
   }
 }
 
+const handleAddressBox = () => {
 
-export const fixHeader = () => document.addEventListener('scroll', handleHeaderScroll);
+  
+  const template = /* html */`
+    <div class="menu_link__address-box">
+      <p><strong>배송지를 등록</strong>하고</p>
+      <p>구매 가능한 상품을 확인하세요!</p>
+      <div class="menu_link__address-box--container">
+        <a href="/src/pages/login/">로그인</a>
+        <button type="button" class="address-box-search"><span></span>주소 검색</button>
+      </div>
+    </div>
+  `
+  if(!isShowAddressBox) {
+    isShowAddressBox = !isShowAddressBox;
+    insertLast(menuLink, template);
+  } else {
+    isShowAddressBox = !isShowAddressBox;
+    menuLink.removeChild(getNode('.menu_link__address-box'));
+  }
+
+}
+
+
+
+
+
+const fixHeader = () => document.addEventListener('scroll', handleScrollHeader);
+const showAddressBox = () => addressButton.addEventListener('click', handleAddressBox);
+
+export const initHeader = () => {
+  fixHeader();
+  showAddressBox();
+}
