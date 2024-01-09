2a362fea0a781bc68f13db5ec0cf86a69b4aae9c
roben31380 <kinalee0430@gmail.com>
2024-01-09 16:27:13 +0900

    feat(feat/kina/#39): 찜,알림버튼 toggle 구현

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,1 +1,20 @@
 import '/src/styles/style.scss';
+import { getNode } from '/src/lib';
+
+const zzimButton = getNode('.zzim');
+const notifyButton = getNode('.notify');
+
+const handleLike = () => {
+  let isLiked = !zzimButton.classList.contains('is-active');
+  zzimButton.classList.toggle('is-active');
+  console.log(`찜: ${isLiked}`);
+};
+
+const handleNotify = () => {
+  let isNotified = !notifyButton.classList.contains('is-active');
+  notifyButton.classList.toggle('is-active');
+  console.log(`알림: ${isNotified}`);
+};
+
+zzimButton.addEventListener('click', handleLike);
+notifyButton.addEventListener('click', handleNotify);
