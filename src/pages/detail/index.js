8b4806e05894ae39f4fa1c8ecd257b0e970dd033
roben31380 <kinalee0430@gmail.com>
2024-01-10 14:03:24 +0900

    feat(feat/kina/#43): 버튼으로 수량을 조절하는 함수 구현

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -5,6 +5,11 @@ initHeader();
 
 const zzimButton = getNode('.zzim');
 const notifyButton = getNode('.notify');
+const countDecrease = getNode('.decrease');
+const countIncrease = getNode('.increase');
+const productCount = getNode('.count');
+const totalPrice = getNode('.product-total-price');
+
 let isClick;
 
 const handleButton = (target) => {
@@ -22,5 +27,23 @@ const handleNotify = () => {
   console.log(`알림 : ${isClick}`);
 };
 
+const setCount = (e) => {
+  let value = e.target.value;
+  let count = +productCount.value;
+  console.log(value, count);
+  if (value === '-') {
+    count -= 1;
+  } else {
+    count += 1;
+  }
+  productCount.value = count;
+  if (productCount.value <= 0) {
+    alert('최소수량은 1개입니다.');
+    productCount.value = 1;
+  }
+};
+
 zzimButton.addEventListener('click', handleLike);
 notifyButton.addEventListener('click', handleNotify);
+countDecrease.addEventListener('click', setCount);
+countIncrease.addEventListener('click', setCount);
