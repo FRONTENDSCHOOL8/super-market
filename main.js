33d1f65b84ffe466ac3962f4a24b961143225d4c
ryukyunghye <ryukyung123@naver.com>
2024-01-10 10:51:57 +0900

    feat(feat/ryu/#48): 메인 페이지 상품 스와이퍼

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 import './src/styles/style.scss';
-import { initHeader, getNode, addClass, removeClass } from '/src/lib';
+import { initHeader, getNode, addClass, removeClass, isString } from '/src/lib';
 
 initHeader();
 
-const bannerSwiper = new Swiper('.banner__cover.swiper-container', {
+const createSwiper = (target, optionList) => {
+  if (isString(target)) getNode(target);
+  return new Swiper(target, {
+    speed: optionList.speed || 500,
+    loop: optionList.loop || false,
+    autoplay: optionList.autoplay || false,
+    slidesPerView: optionList.slidesPerView || 1,
+    spaceBetween: optionList.spaceBetween || 0,
+    slidesPerGroup: optionList.slidesPerGroup || 1,
+    navigation: optionList.navigation || {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
+
+const bannerSwiper = createSwiper('.banner__cover.swiper-container', {
   loop: true,
   autoplay: {
     delay: 3000,
   },
-  speed: 500,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+});
+
+const productSwiper = createSwiper('.product-list.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: '18px',
+  slidesPerGroup: 4,
 });
 
 const getToday = () => {
