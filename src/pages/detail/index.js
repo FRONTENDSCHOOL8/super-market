186a4e9a045c32d4c015c11b9ecb4c430329b065
roben31380 <kinalee0430@gmail.com>
2024-01-10 15:25:06 +0900

    feat(feat/kina/#43): 옵션수량에 맞춰서 총 상품금액을 보여주는 함수 구현

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,5 @@
 import '/src/styles/style.scss';
-import { getNode, initHeader } from '/src/lib';
+import { getNode, initHeader, comma } from '/src/lib';
 
 initHeader();
 
@@ -8,6 +8,7 @@ const notifyButton = getNode('.notify');
 const countDecrease = getNode('.decrease');
 const countIncrease = getNode('.increase');
 const productCount = getNode('.count');
+const optionPrice = getNode('.product-option-price');
 const totalPrice = getNode('.product-total-price');
 
 let isClick;
@@ -27,23 +28,30 @@ const handleNotify = () => {
   console.log(`알림 : ${isClick}`);
 };
 
-const setCount = (e) => {
+const handleCount = (e) => {
   let value = e.target.value;
   let count = +productCount.value;
-  console.log(value, count);
   if (value === '-') {
     count -= 1;
   } else {
     count += 1;
   }
   productCount.value = count;
+
   if (productCount.value <= 0) {
     alert('최소수량은 1개입니다.');
     productCount.value = 1;
   }
+
+  showTotalPrice();
+};
+
+const showTotalPrice = () => {
+  let productPrice = +optionPrice.innerText.replace(/\D/g, '');
+  return (totalPrice.innerText = comma(productPrice * productCount.value));
 };
 
 zzimButton.addEventListener('click', handleLike);
 notifyButton.addEventListener('click', handleNotify);
-countDecrease.addEventListener('click', setCount);
-countIncrease.addEventListener('click', setCount);
+countDecrease.addEventListener('click', handleCount);
+countIncrease.addEventListener('click', handleCount);
