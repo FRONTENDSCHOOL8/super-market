e541a9b30730f97cf638d599a9143de9b9a2fce5
SWLee2973 <kek2973@naver.com>
2024-01-10 23:47:00 +0900

    feat(feat/sang/#59): 네비게이션 메뉴 체크박스 클릭 시 url에 전달하는 기능 구현 및 페이지 재 로드 시 마우스 위치에 따라 헤더의 드롭다운 메뉴가 로드되는 문제 수정

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,4 +1,4 @@
-import { initHeader } from '/src/lib';
+import { initHeader, setStorage, getStorage } from '/src/lib';
 import '/src/styles/style.scss';
 
 initHeader();
@@ -6,6 +6,7 @@ initHeader();
 const navExpandButton = document.querySelectorAll('div[class^="nav-"] > button');
 const navCheckBox = document.querySelectorAll('div[class^="nav-"] input');
 const navCheckCount = {};
+const selectedItemList = {};
 
 const resetButton = document.querySelector('.products-navigation__header button');
 
@@ -29,6 +30,13 @@ const handleExpandNavigation = (e) => {
 
 }
 
+const handleReset = (e) => {
+  const currentUrl = window.location.href;
+
+  location.href = currentUrl.split('filters=')[0];
+
+}
+
 const handleCheckboxCount = (e) => {
   const group = e.target.closest('div[class^="nav-"]');
   const checkBoxGroup = group.classList[0];
@@ -44,28 +52,85 @@ const handleCheckboxCount = (e) => {
   }
 }
 
-const handleReset = (e) => {
-  const currentUrl = window.location.href;
-  console.log(currentUrl);
+const handleCheckboxSelect = (e) => {
 
-  location.href = currentUrl.split('filters=')[0];
+  let valueArray = [];
+  const selectedItem = e.target.closest('input');
+  const group = e.target.closest('div[class^="nav-"]');
+  const checkBoxGroup = group.classList[0].split('nav-')[1];
 
+  if(checkBoxGroup === 'price') {
+    selectedItemList[checkBoxGroup] = selectedItem.id;
+  } else {
+    
+    if(selectedItem.checked) {
+      
+      if(!selectedItemList[checkBoxGroup] || selectedItemList[checkBoxGroup] == []) {
+        valueArray.push(selectedItem.id);
+        selectedItemList[checkBoxGroup] = valueArray;
+      } else {
+        valueArray = typeof selectedItemList[checkBoxGroup] == 'string' ? new Array(1).fill(selectedItemList[checkBoxGroup]) : selectedItemList[checkBoxGroup];
+        valueArray.push(selectedItem.id);
+        selectedItemList[checkBoxGroup] = valueArray;
+      }
+      
+    } else {
+      selectedItemList[checkBoxGroup] = selectedItemList[checkBoxGroup].split(',').filter(item => item != selectedItem.id);
+    }
+    
+  }
+  setCurrentUrl(selectedItemList)
 }
 
+const setCurrentUrl = (itemList) => {
+
+  const filter = itemList
+  let filters = 'filters=';
+  let target = window.location.href;
+
+  for(let key in filter) {
+
+    if(filter[key] == '') continue;
+    filters += `${key}:${filter[key]}|`;
+  }  
+  filters = filters.slice(0, -1);
+
+  location.href = `${target.split('?pages=1')[0]}?pages=1&${filters}`;
+}
+
+const handleSetCheckItem = (e) => {
+  const filters = window.location.search.split('filters=')[1];
+  let needCheckNode;
+  
+  filters.split('|').map(item => {
+    selectedItemList[item.split(':')[0]] = item.split(':')[1];
+  });
+  
+  console.log(selectedItemList);
+
+  for(let key in selectedItemList) {
+    selectedItemList[key].split(',').forEach(item => {
+      needCheckNode = document.querySelector(`input[id=${item}]`)
+      needCheckNode.checked = true;
+      
+    })
+    needCheckNode.closest('ul').classList.add('is--open')
+    needCheckNode.closest('ul').closest('div[class^="nav-"]').querySelector('button img').style.transform = 'rotate(180deg)';
+  }
 
 
+}
 
 Array.from(navExpandButton).forEach(button => {
   button.addEventListener('click', handleExpandNavigation);
 })
 
 Array.from(navCheckBox).forEach(checkbox => {
-  checkbox.addEventListener('change', handleCheckboxCount);
+  checkbox.addEventListener('change', handleCheckboxSelect);
 })
 
 resetButton.addEventListener('click', handleReset);
-
-
+document.addEventListener('DOMContentLoaded', handleSetCheckItem);
 
 
 
