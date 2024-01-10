c034a99870a6189195fa77551ff5e97e0e2e29aa
SWLee2973 <kek2973@naver.com>
2024-01-10 13:13:28 +0900

    feat(feat/sang/#51): 네비게이션 메뉴 펼치기 및 마이크로 애니메이션 적용

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,3 +2,38 @@ import { initHeader } from '/src/lib';
 import '/src/styles/style.scss';
 
 initHeader();
+
+const navExpandButton = document.querySelectorAll('div[class^="nav-"] > button');
+const navCheckBox = document.querySelectorAll('div[class^="nav-"] input[type^="checkbox"]');
+
+
+const handleExpandNavigation = (e) => {
+  let menu = e.target.closest('button').nextElementSibling;
+
+  while(menu.nodeName != 'UL') {
+    menu = menu.nextElementSibling;
+  }
+
+  if(!menu.classList.contains('is--open')) {
+    menu.classList.add('is--open');
+    e.target.querySelector('img').style.transform = 'rotate(180deg)';
+  } else {
+    menu.classList.remove('is--open');
+    e.target.querySelector('img').style.transform = '';
+  }
+
+}
+
+
+
+
+
+
+
+Array.from(navExpandButton).forEach(button => {
+  button.addEventListener('click', handleExpandNavigation);
+})
+
+Array.from(navCheckBox).forEach(checkbox => {
+  
+})
