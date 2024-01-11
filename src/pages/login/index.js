7be39e12d60c31fcb258c489bb840e6e4d1c36ad
BELLORI <bellori729@gmail.com>
2024-01-11 13:20:54 +0900

    feat(feat/kim/#66):로그인 페이지 아이디, 비밀번호 조건 문구 출력 및 sr-only 레이블 추가

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,23 @@
-import { initHeader } from '/src/lib';
+import { initHeader, getNode } from '/src/lib';
 import '/src/styles/style.scss';
 
 initHeader();
+
+// 공통 유효성 검사 함수
+const validateInput = (inputNode, reg) => {
+  const isValid = reg.test(String(inputNode.value).toLowerCase());
+  inputNode.classList.toggle('is--invalid', !isValid);
+};
+
+// 유효성 검사 할 대상 함수
+
+const validationInput = (inputSelector, reg) => {
+  const userInput = getNode(inputSelector);
+  userInput.addEventListener('input', () => validateInput(userInput, reg));
+};
+
+validationInput('#loginUserId', /^[A-Za-z0-9]{6,16}$/);
+validationInput(
+  '#loginUserPw',
+  /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9\s]).{6,16}$/
+);
