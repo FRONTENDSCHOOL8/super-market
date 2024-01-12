cc58a6d4c0703ffc50741457a71a9086f6252599
roben31380 <kinalee0430@gmail.com>
2024-01-12 15:24:54 +0900

    feat(feat/kina/#83): 디테일페이지 동적 렌더링

--- src/lib/components/productCard.js
+++ src/lib/components/productCard.js
@@ -1,14 +1,23 @@
-
 import { getPbImageURL, comma } from '/src/lib';
 
 export const createCardTemplate = (product) => {
-  const {id, product_name, brand_id, price, 
-      discount, product_description, karly_only, limit} = product;
-  
-  const realPrice = comma(Math.floor(price * (1 - (0.01 * discount))/10)*10);
+  const {
+    id,
+    product_name,
+    brand_id,
+    price,
+    discount,
+    product_description,
+    karly_only,
+    limit,
+  } = product;
+
+  const realPrice = comma(
+    Math.floor((price * (1 - 0.01 * discount)) / 10) * 10
+  );
 
   let template = /* html */ `
-    <a href="/src/pages/detail/index#${id}" class="product swiper-slide">
+    <a href="/src/pages/detail/#${id}" class="product swiper-slide">
       <div class="product__images">
         <img
           src="${getPbImageURL(product, 'product_img')}"
@@ -21,64 +30,63 @@ export const createCardTemplate = (product) => {
           class="cart"
         />
       </div>
-    `
-  
-  if(brand_id) {
+    `;
+
+  if (brand_id) {
     template += /* html */ `
       <span class="product__brand">${product_name}</span>
-    `
+    `;
   }
 
   template += /* html */ `
       <span class="product__title">${product_name}</span>
       <p class="product__discount">
-    `
-    if(discount) {
-      template += /* html */ `
-      <span class="product__discount-rate">${discount}%</span>
-      `
-    }
-    
+    `;
+  if (discount) {
     template += /* html */ `
+      <span class="product__discount-rate">${discount}%</span>
+      `;
+  }
+
+  template += /* html */ `
         <span class="product__price">${realPrice}원</span>
       </p>
-      `
-    if(discount) {
-      template += /* html */ `
-        <span class="product__regular-price">${comma(price)}원</span>
-      `
-    }
-    
+      `;
+  if (discount) {
     template += /* html */ `
+        <span class="product__regular-price">${comma(price)}원</span>
+      `;
+  }
+
+  template += /* html */ `
       <span class="product__description">${product_description}</span>
       <p class="product__keyword-list">
-    `
+    `;
 
-    if(karly_only == 1) {
-      template += /* html */ `
-        <span class="product__keyword only">Karly Only</span>
-      `
-    } else if (karly_only == 2) {
-      template += /* html */ `
-        <span class="product__keyword only">희소가치 프로젝트</span>
-      `
-    }
-
-    if(limit) {
-      template += /* html */ `
-        <span class="product__keyword">한정수량</span>
-      `
-    }
+  if (karly_only == 1) {
     template += /* html */ `
+        <span class="product__keyword only">Karly Only</span>
+      `;
+  } else if (karly_only == 2) {
+    template += /* html */ `
+        <span class="product__keyword only">희소가치 프로젝트</span>
+      `;
+  }
+
+  if (limit) {
+    template += /* html */ `
+        <span class="product__keyword">한정수량</span>
+      `;
+  }
+  template += /* html */ `
         </p>
       </a>
-    `
+    `;
 
   return template;
-}
+};
 
 export const createSkeletonCardTemplate = () => {
-
   let template = /* html */ `
     <div class="product skeleton_card">
       <div class="product__images">
@@ -108,7 +116,7 @@ export const createSkeletonCardTemplate = () => {
         <p class="product__keyword-list"></p>
       </div>
     </div>
-  `
+  `;
 
   return template;
-}
+};
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,123 @@
 import '/src/styles/style.scss';
-import { getNode, getNodes, initHeader, comma } from '/src/lib';
+import {
+  getNode,
+  getNodes,
+  initHeader,
+  comma,
+  hideElementNoExist,
+  getPbImageURL,
+  insertFirst,
+  insertBefore,
+} from '/src/lib';
+import pb from '../../lib/api/pocketbase.js';
 
 initHeader();
+hideElementNoExist();
+
+const renderDetailData = async () => {
+  const hash = window.location.hash.slice(1);
+  const detailData = await pb.collection('products').getOne(hash);
+  let {
+    delivery_type,
+    product_name,
+    packaging_type,
+    price,
+    product_description,
+    etc,
+  } = detailData;
+
+  switch (packaging_type) {
+    case '1':
+      packaging_type = '상온';
+      break;
+    case '2':
+      packaging_type = '냉장';
+      break;
+    case '3':
+      packaging_type = '냉동';
+      break;
+  }
+
+  const template = {
+    mainImage: /*html*/ `
+  <figure>
+  <img
+    class="main-image"
+    src="${getPbImageURL(detailData, 'product_img')}"
+    alt="${product_name}"
+  />
+</figure>
+  `,
+    mainDescription: /*html*/ `
+  <p class="delivery">${delivery_type}</p>
+  <h1 class="product-name">${product_name}</h1>
+  <h2 class="product-explanation">${product_description}</h2>
+  <p class="product-price"><span>${comma(price)}</span>원</p>
+  `,
+    detailDescription: /*html*/ `
+  <dt>배송</dt>
+            <dd class="product-detail-delivery">
+              <p>${delivery_type}</p>
+              <p>
+                23시 전 주문시 내일 아침 7시 전 도착 <br />
+                (대구 부산 울산 샛별배송 운영시간 별도 확인)
+              </p>
+            </dd>
+
+            <dt>판매자</dt>
+            <dd class="product-detail-seller"><p>칼리</p></dd>
+
+            <dt>포장타입</dt>
+            <dd class="product-detail-package">
+              <p>${packaging_type}</p>
+              <p>택배배송은 에코 포장이 스티로폼으로 대체됩니다.</p>
+            </dd>
+
+
+
+  `,
+    productOtion: /*html*/ `
+    <p class="product-option-name">${product_name}</p>
+                <div class="product-option-count">
+                  <input
+                    value="-"
+                    class="decrease"
+                    type="button"
+                    aria-label="수량 빼기"
+                  />
+                  <input type="number" min="1" value="1" class="count" />
+                  <input
+                    value="+"
+                    class="increase"
+                    type="button"
+                    aria-label="수량 증가"
+                  />
+                </div>
+                <span class="product-option-price">${comma(price)} 원</span>
+            `,
+    total: /*html*/ `
+    <p>총 상품금액: <span class="product-total-price">${comma(
+      price
+    )}</span>원</p>
+    `,
+  };
+
+  insertFirst('.detail-main', template.mainImage);
+  insertFirst('.main-description', template.mainDescription);
+  insertFirst('.product-detail', template.detailDescription);
+  for (let key in etc) {
+    insertBefore(
+      '.product-option-row',
+      `
+    <dt>${key}</dt>
+              <dd><p>${etc[key]}</p></dd>
+    `
+    );
+  }
+  insertFirst('.product-option-wrapper', template.productOtion);
+  insertFirst('.product-total-wrapper', template.total);
+};
+await renderDetailData();
 
 const zzimButton = getNode('.zzim');
 const notifyButton = getNode('.notify');
@@ -52,6 +168,8 @@ const showTotalPrice = () => {
   let productPrice = +optionPrice.innerText.replace(/\D/g, '');
   return (totalPrice.innerText = comma(productPrice * productCount.value));
 };
+
+console.log(optionPrice);
 
 const handleDetailNav = () => {
   if (!navItem) return;
